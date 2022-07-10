c75f8d98a1e8506df41c72453dbf23b8b013ebad
John Woo <100068435+WooJonnyBoy@users.noreply.github.com>
2022-07-11 01:37:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+(function() {
+    let i = 0
+    let num = document.querySelector('.box')
+    let arrow = document.querySelectorAll('.arrow')
+    let letter = 'абвгґдеєжзиіїйклмнопрстуфхчцшщьюя'
+    let lerarr = Array.from(letter.toUpperCase())
+    num.textContent = lerarr[0]
+    document.addEventListener('touchstart', addletter)
+    function addletter(e) {
+        if(e.target == arrow[0]) {
+            i--
+            if(i < 0) {i = 32}
+            num.textContent = lerarr[i]
+            num.style.color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
+            
+        }
+        if(e.target == arrow[1]) {
+            i++
+            if(i > 32) {i = 0}
+            num.textContent = lerarr[i]
+            num.style.color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
+            
+        }
+        }
+})()
+
+function random(min, max) {
+    let rand = Math.round(min - 0.5 + Math.random() * (max - min + 1))
+    return rand
+}
+
+
+
+
